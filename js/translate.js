6d7e6231225d993d2cf61d132da7bad805a62d07
Taher Shoker <taher.shoker@qeema.net>
2023-05-07 23:58:57 +0300

    handle translate files

--- js/translate.js
+++ js/translate.js
@@ -1,34 +1,39 @@
-function Translate() { 
-	//initialization
-	this.init =  function(attribute, lng){
-		this.attribute = attribute;
-		this.lng = lng;	
-	}
-	//translate 
-	this.process = function(){
-		_self = this;
-		var xrhFile = new XMLHttpRequest();
-		//load content data 
-		xrhFile.open("GET", "lng/"+this.lng+".json", false);
-		xrhFile.onreadystatechange = function ()
-		{
-			if(xrhFile.readyState === 4)
-			{
-				if(xrhFile.status === 200 || xrhFile.status == 0)
-				{
-					var LngObject = JSON.parse(xrhFile.responseText);
-					var allDom = document.getElementsByTagName("*");
-					for(var i =0; i < allDom.length; i++){
-						var elem = allDom[i];
-						var key = elem.getAttribute(_self.attribute);
-						if(key != null) {
-							elem.innerHTML = LngObject[key]  ;
-						}
-					}
-				
-				}
-			}
-		}
-		xrhFile.send();
-    }
+function Translate() {
+  //initialization
+  this.init = function (attribute, lng) {
+    this.attribute = attribute;
+    this.lng = lng;
+  };
+  //translate
+  this.process = function () {
+    _self = this;
+    var xrhFile = new XMLHttpRequest();
+    //load content data
+    xrhFile.open("GET", "lng/" + this.lng + ".json", false);
+    xrhFile.onreadystatechange = function () {
+      if (xrhFile.readyState === 4) {
+        if (xrhFile.status === 200 || xrhFile.status == 0) {
+          var LngObject = JSON.parse(xrhFile.responseText);
+          var allDom = document.getElementsByTagName("*");
+          for (var i = 0; i < allDom.length; i++) {
+            var elem = allDom[i];
+            var key = elem.getAttribute(_self.attribute);
+            if (key != null) {
+              if (key.includes(".")) {
+                var arrStr = key.split(".");
+                var parent = arrStr[0];
+                var child = arrStr[1];
+                var parentObj = LngObject[parent];
+                var childTxt = parentObj[child];
+                elem.innerHTML = childTxt;
+              } else {
+                elem.innerHTML = LngObject[key];
+              }
+            }
+          }
+        }
+      }
+    };
+    xrhFile.send();
+  };
 }
